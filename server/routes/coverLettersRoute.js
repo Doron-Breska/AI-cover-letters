3895f11d84ab3e04d5cd0b7e4a6a4e7628f87fe1
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-09-20 14:15:08 +0200

    new saveNewLetter and saveNewLetter functions

--- server/routes/coverLettersRoute.js
+++ server/routes/coverLettersRoute.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { getAllLetters } from "../controller/coverLettersController.js";
+import {
+  deleteCoverLetter,
+  getAllLetters,
+  saveNewLetter,
+} from "../controller/coverLettersController.js";
+import jwtAuth from "../middlewares/jwtAuth.js";
 
 // import jwtAuth from "../middlewares/jwtAuth.js";
 
 const router = express.Router();
 
 router.get("/all", getAllLetters);
+router.post("/new", jwtAuth, saveNewLetter);
+router.delete("/:id", jwtAuth, deleteCoverLetter);
 
 export default router;
