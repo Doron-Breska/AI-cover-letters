eb8da7cbae4fb38e8b06a4bfa089b74d7cd6d64a
arianecrestani <ariane.mrv@gmail.com>
2023-09-26 15:41:43 +0200

    setup redeux component, fetch a api router from the front

---
+++ client/src/components/Profile.tsx
@@ -0,0 +1,18 @@
+// import { useSelector } from "react-redux";
+// import { RootState } from "../main";
+
+
+
+
+
+// const Profile = () => {
+//   const user = useSelector((state: RootState) => state.user.user);
+//   return (
+//     <div>
+//       <button>Profile PAGE</button>
+  
+//     </div>
+//   );
+// };
+
+// export default Profile;
