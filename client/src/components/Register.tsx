1f5a3d688dd817926e5a1a68f5a36f85d1a1da0b
arianecrestani <ariane.mrv@gmail.com>
2023-10-06 18:17:48 +0200

    add inputs skill, education and work experience

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -2,9 +2,8 @@ import React, { useRef } from "react";
 import axios from "axios";
 import "tailwindcss/tailwind.css";
 
-// import { useDispatch, useSelector } from "react-redux";
-// import { login } from "../slices/userSlice";
-// import { RootState } from "../store/store";
+// Import useState for future use
+// import { useState } from "react";
 
 type Avatar = string | File;
 
@@ -30,52 +29,49 @@ interface PersonalInfo {
   Resilience: string;
   Innovativeness_Creativity: string;
   Emotional_Intelligence: string;
+  tech_info: string; // Added tech_info field here
 }
 
 const Register: React.FC = () => {
-  // const dispatch = useDispatch();
-
   const usernameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const firstNameRef = useRef<HTMLInputElement>(null);
   const lastNameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
-  const techInfoRef = useRef<HTMLTextAreaElement>(null);
   const personalTextRef = useRef<HTMLTextAreaElement>(null);
   const imgRef = useRef<HTMLInputElement>(null);
 
-  const leadershipRef = useRef<HTMLInputElement>(null);
-  const adaptabilityRef = useRef<HTMLInputElement>(null);
-  const proactivityRef = useRef<HTMLInputElement>(null);
-  const attentionToDetailRef = useRef<HTMLInputElement>(null);
-  const spontaneityRef = useRef<HTMLInputElement>(null);
-  const teamworkRef = useRef<HTMLInputElement>(null);
-  const resilienceRef = useRef<HTMLInputElement>(null);
-  const innovativenessRef = useRef<HTMLInputElement>(null);
-  const emotionalIntelligenceRef = useRef<HTMLInputElement>(null);
+  // Add refs for skills, education, and work experience
+  const skillsRef = useRef<HTMLInputElement>(null);
+  const educationRef = useRef<HTMLInputElement>(null);
+  const workExperienceRef = useRef<HTMLInputElement>(null);
+
+  // Add a ref for techInfoRef
+  const techInfoRef = useRef<HTMLTextAreaElement>(null);
 
   // const [errorMessage, setErrorMessage] = useState<string | null>(null);
-
-  // const resetInputs = () => {
-  //   if (usernameRef.current) usernameRef.current.value = "";
-  //   if (emailRef.current) emailRef.current.value = "";
-  //   if (passwordRef.current) passwordRef.current.value = "";
-  //   // Reset other inputs here
-  // };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    const skillsValue = skillsRef.current?.value || "";
+    const educationValue = educationRef.current?.value || "";
+    const workExperienceValue = workExperienceRef.current?.value || "";
+
+    // Concatenate the values into a single string
+    const techInfoValue = `${skillsValue}, ${educationValue}, ${workExperienceValue}`;
+
     const personalInfo: PersonalInfo = {
-      Leadership: leadershipRef.current?.value || "",
-      Adaptability_Flexibility: adaptabilityRef.current?.value || "",
-      Proactivity_Initiative: proactivityRef.current?.value || "",
-      Attention_to_Detail: attentionToDetailRef.current?.value || "",
-      Spontaneity: spontaneityRef.current?.value || "",
-      Teamwork_Collaboration: teamworkRef.current?.value || "",
-      Resilience: resilienceRef.current?.value || "",
-      Innovativeness_Creativity: innovativenessRef.current?.value || "",
-      Emotional_Intelligence: emotionalIntelligenceRef.current?.value || "",
+      Leadership: "", // Add the values as needed
+      Adaptability_Flexibility: "",
+      Proactivity_Initiative: "",
+      Attention_to_Detail: "",
+      Spontaneity: "",
+      Teamwork_Collaboration: "",
+      Resilience: "",
+      Innovativeness_Creativity: "",
+      Emotional_Intelligence: "",
+      tech_info: techInfoValue, // Assign the concatenated value here
     };
 
     const newUser: NewUser = {
@@ -84,7 +80,7 @@ const Register: React.FC = () => {
       password: passwordRef.current?.value || "",
       first_name: firstNameRef.current?.value || "",
       last_name: lastNameRef.current?.value || "",
-      tech_info: techInfoRef.current?.value || "",
+      tech_info: techInfoValue, // Assign the concatenated value here as well
       personal_text: personalTextRef.current?.value || "",
       img: imgRef.current?.files
         ? imgRef.current.files[0]
@@ -107,6 +103,7 @@ const Register: React.FC = () => {
       }
       // Append other fields as needed
 
+      // Replace the API endpoint with your actual endpoint
       const response = await axios.post(
         "http://localhost:5001/api/users/new",
         formData
@@ -129,118 +126,151 @@ const Register: React.FC = () => {
       }
     } catch (error) {
       console.error("Registration Error:", error);
-      // setErrorMessage("Registration Error: " + error.message);
+
     }
   };
 
- return (
-   <div className="min-h-screen flex items-center justify-center bg-gray-100">
-     <form
-       onSubmit={handleSubmit}
-       className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md"
-     >
-       <div className="mb-4">
-         <label className="block text-gray-700 text-sm font-bold mb-2">
-           Username:
-         </label>
-         <input
-           ref={usernameRef}
-           type="text"
-           required
-           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-         />
-       </div>
-       <div className="mb-4">
-         <label className="block text-gray-700 text-sm font-bold mb-2">
-           Email:
-         </label>
-         <input
-           ref={emailRef}
-           type="email"
-           required
-           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-         />
-       </div>
-       <div className="mb-4">
-         <label className="block text-gray-700 text-sm font-bold mb-2">
-           First Name:
-         </label>
-         <input
-           ref={firstNameRef}
-           type="text"
-           required
-           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-         />
-       </div>
-       <div className="mb-4">
-         <label className="block text-gray-700 text-sm font-bold mb-2">
-           Last Name:
-         </label>
-         <input
-           ref={lastNameRef}
-           type="text"
-           required
-           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-         />
-       </div>
-       <div className="mb-4">
-         <label className="block text-gray-700 text-sm font-bold mb-2">
-           Password:
-         </label>
-         <input
-           ref={passwordRef}
-           type="password"
-           required
-           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-         />
-       </div>
-       <div className="mb-4">
-         <label className="block text-gray-700 text-sm font-bold mb-2">
-           Tech Info:
-         </label>
-         <textarea
-           ref={techInfoRef}
-           required
-           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-         ></textarea>
-       </div>
-       <div className="mb-4">
-         <label className="block text-gray-700 text-sm font-bold mb-2">
-           Personal Text:
-         </label>
-         <textarea
-           ref={personalTextRef}
-           required
-           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-         ></textarea>
-       </div>
-       <div className="mb-4">
-         <label className="block text-gray-700 text-sm font-bold mb-2">
-           Image URL:
-         </label>
-         <input
-           ref={imgRef}
-           type="file"
-           required
-           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-         />
-       </div>
-       {/* Personal Info Fields */}
-       {/* Add styling for personal info fields here */}
-       <div className="mb-4">
-         <button
-           type="submit"
-           onClick={() => {
-             handleSubmit;
-           }}
-           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
-         >
-           Create user
-         </button>
-       </div>
-     </form>
-   </div>
- );
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-md"
+      >
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Username:
+          </label>
+          <input
+            ref={usernameRef}
+            type="text"
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Email:
+          </label>
+          <input
+            ref={emailRef}
+            type="email"
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            First Name:
+          </label>
+          <input
+            ref={firstNameRef}
+            type="text"
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Last Name:
+          </label>
+          <input
+            ref={lastNameRef}
+            type="text"
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Password:
+          </label>
+          <input
+            ref={passwordRef}
+            type="password"
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        {/* Add three input fields for skills, education, and work experience */}
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Skills:
+          </label>
+          <input
+            ref={skillsRef}
+            type="text"
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Education:
+          </label>
+          <input
+            ref={educationRef}
+            type="text"
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Work Experience:
+          </label>
+          <input
+            ref={workExperienceRef}
+            type="text"
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Tech Info:
+          </label>
+          <textarea
+            ref={techInfoRef} 
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          ></textarea>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Personal Text:
+          </label>
+          <textarea
+            ref={personalTextRef}
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          ></textarea>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Image URL:
+          </label>
+          <input
+            ref={imgRef}
+            type="file"
+            required
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+
+        <div className="mb-4">
+          <button
+            type="submit"
+            onClick={() => {
+              handleSubmit;
+            }}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+          >
+            Create user
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
