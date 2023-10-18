63ba39221cec91299bfd810dd6262dae0d61f5de
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-10-18 22:52:40 +0200

    new online db aws postgres

--- server/pgConfig.js
+++ server/pgConfig.js
@@ -2,12 +2,14 @@ import PG from "pg";
 const Pool = PG.Pool;
 
 const pool = new Pool({
-
-  user: "postgres", // Change this to the appropriate user if needed
-  host: "localhost", // Use "localhost" because it's running locally
-  database: "postgres", // Change this to the appropriate database name
-  password: "", // You may need to specify a password if one is set
-  port: 5433, // Use the correct port (5433 in your case)
+  user: "doron",
+  host: "database-letters.cd3cu8asae7s.eu-central-1.rds.amazonaws.com",
+  database: "letters_db",
+  password: "Dbrd-11223344",
+  port: 5455,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
