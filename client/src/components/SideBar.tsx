ff7d995fa64ad4f7e3f04d863e58fb593b295655
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-10-18 17:37:07 +0200

    test - login sidebar url

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -68,7 +68,7 @@ const SideBar: React.FC = () => {
       setMsg("");
 
       const coverLetterResponse = await axios.get(
-        `${serverURL}/api/c-l/user/`,
+        `https://ai-cover-letters-server.vercel.app/api/c-l/user/`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
