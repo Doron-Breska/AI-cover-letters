ae989ff0c2c4d6e9c02231c7e1acf7da0ea42c6a
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-10-18 17:14:55 +0200

    updated serverURL

--- client/src/utils/serverURL.tsx
+++ client/src/utils/serverURL.tsx
@@ -1,6 +1,6 @@
 const serverURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5001"
-    : "https://mern-project-succulents-server.vercel.app";
+    : "https://ai-cover-letters-server.vercel.app/";
 
 export { serverURL };
