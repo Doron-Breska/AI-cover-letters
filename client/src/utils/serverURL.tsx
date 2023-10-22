9c6ef12f287aa67b201927140555859f1e38d905
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-10-22 21:48:09 +0200

    serverURL

--- client/src/utils/serverURL.tsx
+++ client/src/utils/serverURL.tsx
@@ -1,6 +1,6 @@
 const serverURL =
   import.meta.env.VITE_APP_ENV === "development"
     ? "http://localhost:5001"
-    : "https://ai-cover-letters-virid.vercel.app/";
+    : "https://ai-cover-letters-virid.vercel.app";
 
 export { serverURL };
