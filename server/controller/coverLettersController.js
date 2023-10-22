370f181611b4a6d3e616c19cf8ad91e1c546c84c
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-10-22 13:53:29 +0200

    removed logs

--- server/controller/coverLettersController.js
+++ server/controller/coverLettersController.js
@@ -97,7 +97,7 @@ const createNewLetter = async (req, res) => {
     const combinedUserInfo = `${techInfo}. ${personalText}`;
 
     // const data = await openAi(combinedUserInfo, personalInfo, combinedJobInfo);
-    console.log("these are the variables :", combinedUserInfo, combinedJobInfo);
+    // console.log("these are the variables :", combinedUserInfo, combinedJobInfo);
     const data = await openAi(combinedUserInfo, combinedJobInfo);
     // console.log("Data received in createNewLetter:", data);
 
