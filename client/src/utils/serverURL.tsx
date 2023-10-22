c5680d9f84c392ed79fbe0cb31d1c68e619ba713
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2023-10-22 18:07:24 +0200

    updated serverURL

--- client/src/utils/serverURL.tsx
+++ client/src/utils/serverURL.tsx
@@ -1,6 +1,6 @@
 const serverURL =
   import.meta.env.VITE_APP_ENV === "development"
     ? "http://localhost:5001"
-    : "http://new-ai-cover-letters-server.vercel.app";
+    : "https://ai-cover-letters-server.vercel.app";
 
 export { serverURL };
